c5183dad421e7ebc06efa9dc527b43f778869aaf
Arifblogger77 <arifblogger77@gmail.com>
2023-05-11 17:02:04 +0700

    day 1

---
+++ src/scripts/views/app.js
@@ -0,0 +1,16 @@
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {
+    // TODO: initial Drawer
+    // kita bisa menginisiasikan komponen lain bila ada
+  }
+}
+
+export default App;

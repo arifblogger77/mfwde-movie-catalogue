73eaad7ab54984438c83813139bdb4e8ab8dac96
Arifblogger77 <arifblogger77@gmail.com>
2023-05-19 16:59:35 +0700

    cache api

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  KEY: 'b516ae33196c57de7df29d3ef148dd16',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  // CACHE_NAME: 'MovieCatalogue-V1',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,3 +1,7 @@
+import routes from '../routes/routes';
+import UrlParser from '../routes/url-parser';
+import DrawerInitiator from '../utils/drawer-initiator';
+
 class App {
   constructor({ button, drawer, content }) {
     this._button = button;
@@ -8,8 +12,20 @@ class App {
   }
 
   _initialAppShell() {
-    // TODO: initial Drawer
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+
     // kita bisa menginisiasikan komponen lain bila ada
+  }
+
+  async renderPage() {
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    const page = routes[url];
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
   }
 }
 
